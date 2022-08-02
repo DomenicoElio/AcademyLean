b030ab634f5554e55ba4c4dda1cdbc8958ed0793
Domenico <domenico.bressanello@avanade.com>
2022-08-02 14:49:40 +0200

    Additional changes - conversion method

--- WorkbookLean-Academy/Program.cs
+++ WorkbookLean-Academy/Program.cs
@@ -4,7 +4,15 @@ class Test                      //Class Declaration
 {
     static void Main()          //Method Declaration
     {
-        int var = 15 * 10;      //Declearing and Initializing variable 
-        Console.WriteLine($"Your Variable is worth: {var}"); //Print to Console command
+        /*int var = 15 * 10;      //Declearing and Initializing variable 
+        Console.WriteLine($"Your Variable is worth: {var}"); //Print to Console command*/
+        Console.WriteLine($"Your conversion result is: {FeetToInches(15)}");  //calling method and passing the feet value needed: 225
+        Console.WriteLine($"Your conversion result is: {FeetToInches(150)}"); //calling method and passing the feet value needed: 2250
+    }
+
+    static int FeetToInches(int feet)
+    { //creating method that converts feet to inch: accept feet (int feet) as imput
+        int inches = feet * 15;
+        return inches;                  // returns inches
     }
 }                               // end of Class
