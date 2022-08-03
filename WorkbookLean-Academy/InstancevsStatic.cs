f5e1a67f195016d33f1e8710914a208f9a16514e
Domenico <domenico.bressanello@avanade.com>
2022-08-03 13:16:29 +0200

    InstanceVsStatic class members - sample with Human class and explanation

---
+++ WorkbookLean-Academy/InstancevsStatic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkbookLean_Academy
+{
+    public class Human
+    {
+
+        public string Name; // property per each human (instance field - must be instanciated whenever i create a new instance of "Human" or obj)
+        public static int Population; //properties of the human class (static field - does not need to be instanciated every time i create an obj, its instrinsic to the class)
+
+        public Human(string n)   // constructor of Human class
+        { Name = n; Population = Population + 1; } //per each human created increment static value of population
+
+    }
+}
--- WorkbookLean-Academy/Program.cs
+++ WorkbookLean-Academy/Program.cs
@@ -1,15 +1,16 @@
 ﻿using System; //Importing Namespace
+using WorkbookLean_Academy;
 
-public class UnitConverter
+/*public class UnitConverter
 {
     int ratio; //declearing variable
     public UnitConverter(int unitRatio) { ratio = unitRatio; }
     public int Convert(int unit) { return unit * ratio; } 
-}
+}*/
 
 class Test                      //Class Declaration
 {
-     static void Main()          //Method Declaration
+    static void Main()          //Method Declaration
     {
         /*int var = 15 * 10;      //Declearing and Initializing variable 
         Console.WriteLine($"Your Variable is worth: {var}"); //Print to Console command
@@ -23,7 +24,7 @@ class Test                      //Class Declaration
         return inches;                  // returns inches
     }*/
 
-        string name = "domenico";    //first use of strings to store name
+        /*string name = "domenico";    //first use of strings to store name
         string upperCase = name.ToUpper(); // calling the String method to Pascal case (upper first words) 
         Console.WriteLine($"My name is: {upperCase}"); //print name with upper
 
@@ -45,6 +46,15 @@ class Test                      //Class Declaration
 
         Console.WriteLine(feetToInchesCon.Convert(30)); //calling obj and method with .method
         Console.WriteLine(feetToInchesCon.Convert(300));//calling obj and method with .method
-        Console.WriteLine(feetToInchesCon.Convert(milesToFeetCon.Convert(15)));
+        Console.WriteLine(feetToInchesCon.Convert(milesToFeetCon.Convert(15)));*/
+
+        Human dom = new Human("Domenico"); // created instances of my Human
+        Human jack = new Human("Giacomo"); //created instances of my Human 
+
+        Console.WriteLine($"The name of the first born son is: {dom.Name}"); // print human name
+        Console.WriteLine($"The name of his brother is: {jack.Name}"); //print human name
+
+        Console.WriteLine($"The total number of sons is: {Human.Population}"); //satic field has updated alongside number increase of obj thanks to: ({ Name = n; Population = Population + 1; })
+
     }
 }                               // end of Class
