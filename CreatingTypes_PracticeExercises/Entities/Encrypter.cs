c0a519774f58051f12a4041392121b9dd3f9bd5d
Domenico <domenico.bressanello@avanade.com>
2022-08-31 13:02:39 +0200

    progress on encrypt and decrypt functions - general progress on projects)

--- CreatingTypes_PracticeExercises/Entities/Encrypter.cs
+++ CreatingTypes_PracticeExercises/Entities/Encrypter.cs
@@ -1,23 +1,32 @@
 ﻿using CreatingTypes_PracticeExercises.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace CreatingTypes_PracticeExercises.Entities
 
 {
-    internal class Encrypter : IEncrypter
+    public class Encrypter : IEncrypter
     {
-        public string Decrypt(string decpryted)
-        {
-            throw new NotImplementedException();
+        public string Decrypt(string decrypt) {
+            Console.WriteLine("Insert the word you would like to decrypt: ");
+            decrypt = Console.ReadLine();
+
+            decrypt.Split(' ');
+
+            for (int i=0; i<decrypt.Length; i++) {
+                Debug.Assert (decrypt.Length == 1 && Regex.IsMatch(decrypt, "[a-yA-y]"));
+                var next = (char)(decrypt[0] + 1); 
+                next.ToString(); }
+
+            return decrypt;
         }
 
-        public string Encrypt(string encrypted)
-        {
-            throw new NotImplementedException();
-        }
+        public string Encrypt(string encrypt) { throw new NotImplementedException();  }
+
     }
 }
--- CreatingTypes_PracticeExercises/Interfaces/IEncrypter.cs
+++ CreatingTypes_PracticeExercises/Interfaces/IEncrypter.cs
@@ -8,7 +8,7 @@ namespace CreatingTypes_PracticeExercises.Interfaces
 {
     public interface IEncrypter
     {
-        public string Encrypt(string encrypted);
-        public string Decrypt(string decpryted);
+        public string Encrypt(string encrypt);
+        public string Decrypt(string decrypt);
     }
 }
