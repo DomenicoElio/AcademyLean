264961535653545a9a5816056cf88dab31b1576f
Domenico <domenico.bressanello@avanade.com>
2022-09-02 16:41:22 +0200

    small changes especially to exercises - creation of house obj

--- CreatingTypes_PracticeExercises/Program.cs
+++ CreatingTypes_PracticeExercises/Program.cs
@@ -4,14 +4,11 @@ using CreatingTypes_PracticeExercises.Entities;
 class Academy {
     static void Main() {
 
-        SmallApartment miniscule = new SmallApartment() { Area = 50 };
+        SmallApartment miniscule = new SmallApartment() { };
         //simplified declaration of new obj from person class with implementation of name attribute
         
-        
-        //longer declearation of obj with implementation of area  - keeping both to remember both
-
-        
-        //calling ShowData method from the class person
+        miniscule.Area = 50; // assigning 50 to area value for miniscule obj
+        miniscule.ShowData(); //caling method ShowData of House
 
         Encrypter mario = new Encrypter();
         mario.Decrypt();
